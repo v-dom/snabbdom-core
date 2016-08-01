9fc71b09ad48cf07b9774315dad656700581a52f
suxxus <suxxus@outlook.com>
2016-08-01 22:13:11 +0200

    initial

---
+++ src/scripts/core/index.js
@@ -0,0 +1,68 @@
+'use strict';
+
+var snabbdom = require('snabbdom');
+var h = require('snabbdom/h');
+
+
+module.exports = {
+
+    patch: snabbdom.init([]),
+
+    H: h('div.hello-snabbdom', {
+        style: { color: '#000' }
+    }, 'hello again'),
+
+    doPatch: function(vnode) {
+        var newVnode = h('div', 'Hello Snabbdom');
+        var patch = snabbdom.init([]);
+        return patch(vnode, newVnode);
+    },
+
+    withHooks: function(vnode) {
+        // Hooks are a way to hook into the lifecycle of DOM nodes.
+        // Snabbdom offers a rich selection of hooks.
+        // Hooks are used both by modules to extend Snabbdom,
+        // and in normal code for executing arbitrary code at desired points
+        // in the life of a virtual node.
+
+        var newVnode = h('div.with-hooks', {
+
+            hook: {
+
+                init: function() {
+                    // This hook is invoked during the patch process
+                    // when a new virtual node has been found.
+                    // The hook is called before Snabbdom has processed the node
+                    // in any way. I.e., before it has created a DOM node based on
+                    // the vnode.
+                    console.log('init hook was invoked');
+                },
+
+                insert: function() {
+                    // This hook is invoked once the DOM element
+                    // for a vnode has been inserted into the document
+                    // and the rest of the patch cycle is done.
+                    console.log('insert hook was invoked');
+                },
+
+                remove: function(vn, callback) {
+                    // Allows you to hook into the removal of an element.
+                    // The hook is called once a vnode is to be removed from the DOM.
+                    // The handling function receives both the vnode and a callback.
+                    // You can control and delay the removal with the callback.
+                    // The callback should be invoked once the hook is done doing its business,
+                    // and the element will only be removed once all remove hooks
+                    console.log('remove hook was invoked. Text is', vn.text);
+
+                    setTimeout(function() {
+                        console.log('element was removed');
+                        return callback();
+                    }, 1000);
+                }
+
+            }
+        }, 'with hooks');
+        var patch = snabbdom.init([]);
+        return patch(vnode, newVnode);
+    }
+};
---
+++ src/tests/scripts/core/index.js
@@ -0,0 +1,79 @@
+'use strict';
+
+var test = require('tape');
+var benv = require('benv');
+var snabbdom = require('scripts/core');
+
+
+var before = test;
+
+before('description: snabdom core', function(t) {
+
+    benv.setup(function() {
+        window.document.querySelector('body').innerHTML = `<div id="container">
+                                                              <div id="placeholder"></div>
+                                                              <div id="with-hooks"></div>
+                                                           </div>`;
+        t.end();
+    });
+});
+
+test('init', function(t) {
+    // The core exposes only one single function snabbdom.init.
+    // This init takes a list of modules and returns a patch
+    // function that uses the specified set of modules.
+
+    var actual = typeof snabbdom.patch,
+        expect = 'function';
+    t.equal(actual, expect, 'returns a `patch` function');
+    t.end();
+});
+
+test('h', function(t) {
+    // It is recommended that you use snabbdom/h
+    // to create vnodes. h accepts a tag/selector as a string,
+    // an optional data object and an optional string or array of children.
+
+    var actual = typeof snabbdom.H,
+        expect = 'object';
+    t.equal(actual, expect, 'snabbdom `h` object');
+    t.end();
+});
+
+test('patch', function(t) {
+    // The patch function returned by init takes two arguments.
+    // The first is a DOM element or a vnode representing the current view.
+    // The second is a vnode representing the new, updated view.
+    var domElm = document.querySelector('#placeholder');
+
+    var actual = snabbdom.doPatch(domElm).elm.innerHTML,
+        expect = 'Hello Snabbdom';
+    t.equal(actual, expect, 'was replaced by the created DOM');
+    t.end();
+});
+
+test('Hooks', function(t) {
+
+    var vnode = document.querySelector('#with-hooks');
+    var actual, expect;
+    vnode = snabbdom.withHooks(vnode);
+
+    actual = vnode.text;
+    expect = 'with hooks';
+    t.equal(actual, expect);
+
+    var patch = snabbdom.patch;
+    vnode = patch(vnode, snabbdom.H);
+
+    actual = document.querySelector('.with-hooks').innerHTML;
+    expect = 'with hooks';
+    t.equal(actual, expect, 'is not removed yet');
+
+    setTimeout(function() {
+        actual = document.querySelector('.hello-snabbdom').innerHTML;
+        expect = 'hello again';
+        t.equal(actual, expect, 'now element was removed');
+    }, 1002);
+
+    t.end();
+});
